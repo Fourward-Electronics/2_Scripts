051296e3b7115801bdb6e6a0510f63ed931b11c2
Sierra <maldonado.sierra.a@gmail.com>
2023-02-28 16:29:24 -0500

    homework

---
+++ File-Delete.ps1
@@ -0,0 +1,11 @@
+# Delete files securely
+
+#Var
+$filename = Read-Host "Enter File Name"
+$location = Read-host "Enter File Location"
+$extention = Read-Host "Enter file extention"
+$filestodelete = Get-ChildItem $location -filer "*$filename*$extention"
+
+foreach ($file in $filestodelete) {
+    Remove-item $file.fullname
+}
