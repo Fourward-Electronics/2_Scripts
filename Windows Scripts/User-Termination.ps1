2e529f7583390b1d1ba7959b7a964aca7b25b74d
Sierra <maldonado.sierra.a@gmail.com>
2023-02-28 12:41:56 -0500

    homework

--- Windows Scripts/User-Termination.ps1
+++ Windows Scripts/User-Termination.ps1
@@ -1,1 +1,5 @@
-Remove-LocalUser -Name "users name"
+# Prompt the user for the username
+$Username = Read-Host "Enter the username to delete"
+
+# Remove the user account
+Remove-LocalUser -Name $Username
