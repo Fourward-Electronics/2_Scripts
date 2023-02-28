020880ac5535ecc99258e9556890f67dc8187c40
Sierra <maldonado.sierra.a@gmail.com>
2023-02-28 13:09:11 -0500

    homework

--- Windows Scripts/Auto-User.ps1
+++ Windows Scripts/Auto-User.ps1
@@ -4,5 +4,15 @@ $Password = Read-Host "Enter the password for the new user" -AsSecureString
 $FullName = Read-Host "Enter the full name for the new user"
 $Description = Read-Host "Enter a description for the new user"
 
+#Check for username
+$userExists = Get-LocalUser -Name $Username -ErrorAction SilentlyContinue
+if ($null -ne $userExists) {
+    Write-Host "User account already exists"
+}
+else {
+    # Create the new user account
+    New-LocalUser -Name $Username -Password $Password -FullName $FullName -Description $Description
+    Write-Host "User account created successfully"
+}
 # Create the new user account
 New-LocalUser -Name $Username -Password $Password -FullName $FullName -Description $Description
