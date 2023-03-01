964756465fb170a2e06313a858f2508e86bdbda1
Dericus Horner <dericushorner@Dericuss-MacBook-Air.local>
2023-03-01 16:39:28 -0500

    Pull System Drive Command Complete

---
+++ Ubuntu Scripts/PullSys4Drives.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Fourward Electronics
+
+#Purpose: To create a bash script that pulls system information to see what drives are available
+
+
+# Main 
+
+echo "Available drives:"
+lsblk 
+chmod +x PullSys4Drives.sh
+
+# Use ./PullSus4Drives.sh to execute in the bash terminal
+# Done
