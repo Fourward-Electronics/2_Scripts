7ed4a69755e7f98ae2f3e5e12b41248f83667972
Sierra <maldonado.sierra.a@gmail.com>
2023-03-01 16:38:22 -0500

    homework

--- Windows Scripts/Auto-User.ps1
+++ Windows Scripts/Auto-User.ps1
@@ -1,16 +1,6 @@
 # Prompt the user to enter the new user details
 $Username = Read-Host "Enter the username for the new user"
 $Password = Read-Host "Enter the password for the new user" -AsSecureString
-$FullName = Read-Host "Enter the full name for the new user"
-$Description = Read-Host "Enter a description for the new user"
 
-#Check for username
-$userExists = Get-LocalUser -Name $Username -ErrorAction SilentlyContinue
-if ($null -ne $userExists) {
-    Write-Host "User account already exists"
-}
-else {
-    # Create the new user account
-    New-LocalUser -Name $Username -Password $Password -FullName $FullName -Description $Description
-    Write-Host "User account created successfully"
-}
+
+Net user $Username $Password
--- Windows Scripts/User-Termination.ps1
+++ Windows Scripts/User-Termination.ps1
@@ -1,11 +1,4 @@
 # Ask for a username
 $Username = Read-Host "Enter the username to delete"
 
-# Check for user
-if ($null -eq $Username) {
-    Write-host "Useraccount does not exist"
-}
-else {
-    Remove-LocalUser -Name $Username
-    Write-Host "User account was removed"
-}
+Net user $Username /delete
